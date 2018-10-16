51bd1e8badc27a41416bacca91b8203be91eebcd
Natalie Abrams <nabrams94@gmail.com>
2018-10-15 18:42:23 -0700

    shorten time between guesses

--- chatServer.js
+++ chatServer.js
@@ -52,52 +52,52 @@ function bot(data, socket, questionNum) {
   var waitTime;
   /// These are the main statments that make up the conversation.
   if (questionNum == 0) {
-    answer = 'Hello ' + input + '. Let\'s play a guessing game!I am going to pick a number between 1 and 100.'; // output response
+    answer = 'Hello ' + input + '. Let\'s play a guessing game! I am going to pick a number between 1 and 100.'; // output response
     waitTime = 5000;
     question = 'You have 5 guesses! What is your first guess?'; // load next question
   }
   else if (questionNum == 1 && input!=number) {
     if (input > number){
       answer = 'You guessed too high!';
-      waitTime = 5000;
+      waitTime = 3000;
       question = 'You have 4 guesses left!! What is your next guess?';
     }
     else{
       answer = 'You guessed too low!';
-      waitTime = 5000;
+      waitTime = 3000;
       question = 'You have 4 guesses left!! What is your next guess?';
     }
   } else if (questionNum == 2 && input!=number) {
     if (input > number){
       answer = 'You guessed too high!';
-      waitTime = 5000;
+      waitTime = 3000;
       question = 'You have 3 guesses left!! What is your next guess?';
     }
     else{
       answer = 'You guessed too low!';
-      waitTime = 5000;
+      waitTime = 3000;
       question = 'You have 3 guesses left!! What is your next guess?';
     }
   } else if (questionNum == 3 && input!=number) {
     if (input > number){
       answer = 'You guessed too high!';
-      waitTime = 5000;
+      waitTime = 3000;
       question = 'You have 2 guesses left!! What is your next guess?';
     }
     else{
       answer = 'You guessed too low!';
-      waitTime = 5000;
+      waitTime = 3000;
       question = 'You have 2 guesses left!! What is your next guess?';
     }
   } else if (questionNum == 4 && input!=number) {
     if (input > number){
       answer = 'You guessed too high!';
-      waitTime = 5000;
+      waitTime = 3000;
       question = 'You have 1 guess left!! What is your LAST guess?';
     }
     else{
       answer = 'You guessed too low!';
-      waitTime = 5000;
+      waitTime = 3000;
       question = 'You have 1 guess left!! What is your LAST guess?';
     }
   } else {
