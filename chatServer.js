7524acec8d7a749d0e723ac538ad5aa715f4595a
Natalie Abrams <nabrams94@gmail.com>
2018-10-15 18:37:00 -0700

    my own chat code!

--- chatServer.js
+++ chatServer.js
@@ -30,8 +30,8 @@ io.on('connect', function(socket) {
   var questionNum = 0; // keep count of question, used for IF condition.
   socket.on('loaded', function() { // we wait until the client has loaded and contacted us that it is ready to go.
 
-    socket.emit('answer', "Hey, hello I am \"___*-\" a simple chat bot example."); //We start with the introduction;
-    setTimeout(timedQuestion, 5000, socket, "What is your name?"); // Wait a moment and respond with a question.
+    socket.emit('answer', "Hey, welcome to the game! Let's get started :)"); //We start with the introduction;
+    setTimeout(timedQuestion, 4000, socket, "First things first! What is your name?"); // Wait a moment and respond with a question.
 
   });
   socket.on('message', (data) => { // If we get a new message from the client we process it;
@@ -43,52 +43,74 @@ io.on('connect', function(socket) {
   });
 });
 //--------------------------CHAT BOT FUNCTION-------------------------------//
+var number= Math.floor(Math.random() * 100) + 1;
+console.log(number);
 function bot(data, socket, questionNum) {
   var input = data; // This is generally really terrible from a security point of view ToDo avoid code injection
   var answer;
   var question;
   var waitTime;
-
   /// These are the main statments that make up the conversation.
   if (questionNum == 0) {
-    answer = 'Hello ' + input + ' :-)'; // output response
+    answer = 'Hello ' + input + '. Let\'s play a guessing game!I am going to pick a number between 1 and 100.'; // output response
     waitTime = 5000;
-    question = 'How old are you?'; // load next question
-  } else if (questionNum == 1) {
-    answer = 'Really, ' + input + ' years old? So that means you were born in: ' + (2018 - parseInt(input)); // output response
-    waitTime = 5000;
-    question = 'Where do you live?'; // load next question
-  } else if (questionNum == 2) {
-    answer = 'Cool! I have never been to ' + input + '.';
-    waitTime = 5000;
-    question = 'Whats your favorite color?'; // load next question
-  } else if (questionNum == 3) {
-    answer = 'Ok, ' + input + ' it is.';
-    socket.emit('changeBG', input.toLowerCase());
-    waitTime = 5000;
-    question = 'Can you still read the font?'; // load next question
-  } else if (questionNum == 4) {
-    if (input.toLowerCase() === 'yes' || input === 1) {
-      answer = 'Perfect!';
+    question = 'You have 5 guesses! What is your first guess?'; // load next question
+  }
+  else if (questionNum == 1 && input!=number) {
+    if (input > number){
+      answer = 'You guessed too high!';
       waitTime = 5000;
-      question = 'Whats your favorite place?';
-    } else if (input.toLowerCase() === 'no' || input === 0) {
-      socket.emit('changeFont', 'white'); /// we really should look up the inverse of what we said befor.
-      answer = ''
-      question = 'How about now?';
-      waitTime = 0;
-      questionNum--; // Here we go back in the question number this can end up in a loop
-    } else {
-      question = 'Can you still read the font?'; // load next question
-      answer = 'I did not understand you. Could you please answer "yes" or "no"?'
-      questionNum--;
-      waitTime = 5000;
+      question = 'You have 4 guesses left!! What is your next guess?';
     }
-    // load next question
+    else{
+      answer = 'You guessed too low!';
+      waitTime = 5000;
+      question = 'You have 4 guesses left!! What is your next guess?';
+    }
+  } else if (questionNum == 2 && input!=number) {
+    if (input > number){
+      answer = 'You guessed too high!';
+      waitTime = 5000;
+      question = 'You have 3 guesses left!! What is your next guess?';
+    }
+    else{
+      answer = 'You guessed too low!';
+      waitTime = 5000;
+      question = 'You have 3 guesses left!! What is your next guess?';
+    }
+  } else if (questionNum == 3 && input!=number) {
+    if (input > number){
+      answer = 'You guessed too high!';
+      waitTime = 5000;
+      question = 'You have 2 guesses left!! What is your next guess?';
+    }
+    else{
+      answer = 'You guessed too low!';
+      waitTime = 5000;
+      question = 'You have 2 guesses left!! What is your next guess?';
+    }
+  } else if (questionNum == 4 && input!=number) {
+    if (input > number){
+      answer = 'You guessed too high!';
+      waitTime = 5000;
+      question = 'You have 1 guess left!! What is your LAST guess?';
+    }
+    else{
+      answer = 'You guessed too low!';
+      waitTime = 5000;
+      question = 'You have 1 guess left!! What is your LAST guess?';
+    }
   } else {
-    answer = 'I have nothing more to say!'; // output response
+  if (input!=number){
+    answer = 'You lose :( The number was '+number ; // output response
     waitTime = 0;
     question = '';
+  }
+  else{
+    answer = 'YOU WIN!!!'; // output response
+    waitTime = 0;
+    question = '';
+  }
   }
 
 
